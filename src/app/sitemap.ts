eb6651a7c6216e1fb01b5f9887f80814146472f8
367ifast-spec <367ifast@gmail.com>
2026-07-17 22:25:59 +0600

    Add US LLC vs UK LTD blog to sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = "https://www.vanguardbusinesservices.com";
+  const baseUrl = "https://www.vanguardbusineservices.com";
 
   const now = new Date();
 
